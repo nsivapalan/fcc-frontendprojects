e68526e3833b79ee830b5c932acec62c6d78a906
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2017-01-09 17:08:23 -0800

    update CSS and script to get search working

--- wiki_viewer/script.js
+++ wiki_viewer/script.js
@@ -1,35 +1,43 @@
 $(document).ready(function() {
 
-	var source = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&callback=?&search=cricket";
-	// var source = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&callback=?&srsearch="
+	var original = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&callback=?&search=";
+	var source = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&callback=?&search=";
 	var searchItem = "";
 
-	$("#find").click(getResults(source));
-
-	// $("#find").on("click", function() {
-	//   searchItem = document.getElementById("searchItem").value;
-	//   source += searchItem;
-	//   console.log(source);
-	//   getResults(source);
-	// });
+	$("#find").on("click", function() {
+		source = original;
+		$("#searchResults").html("");
+		if ($("#searchItem").val().length === 0) {
+			alert("Please enter a value!");
+		} else {
+			searchItem = document.getElementById("searchItem").value;
+			console.log(searchItem);
+			source += searchItem;
+			getResults(source);
+		}
+	});
 
 	function getResults(source) {
 	  console.log("The source is " + source);
-	  console.log(source);
 	  $.getJSON(source, function(data) {
-	    console.log("Json activate");
 	    console.log(data);
-	    for (var i = 0; i < 10; i++) {
-	    	addResult(data[1][i], data[2][i], data[3][i]);
-	    }
+	    if (data[1].length === 0) {
+	    	noResults();
+	    } else {
+		    	for (var i = 0; i < data[1].length; i++) {
+	    			addResult(data[1][i], data[2][i], data[3][i]);
+	    		}
+	    	}
 	  });
 	}
 
 	function addResult(title, des, link) {
+		$("#searchResults").append("<li>" + "<a target='_blank' href='" + link + "'>" + "<h4>" + title
+		+ "</h4>" + "</a>" + des + "</li>");
+	}
 
-		$("#searchResults").append("<li>" + "<a target='_blank' href='" + link + "'>" + "<h3>" + title
-		+ "</h3>" + "</a>" + "<br>" + des + "</li>");
-
+	function noResults() {
+		$("#searchResults").append("<li><h4>No results. Please try again!</h4></li>");		
 	}
 
 });
