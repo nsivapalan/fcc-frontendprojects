89c4445263f74dc01b2c321242fe2fb7bc49f47e
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2017-01-12 13:20:49 -0800

    update twitch api to work

--- twitch_streams/script.js
+++ twitch_streams/script.js
@@ -3,7 +3,14 @@ $(document).ready(function() {
 								 "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
 
 	var clientID = config.CLIENT_ID;
-	var source;
+
+	var headers = {
+		"accept": "application/vnd.twitchtv.v5+json",
+		"client_id": clientID
+	}
+
+	var source = "https://api.twitch.tv/kraken/channels/freecodecamp?client_id=" + clientID + "&accept" + headers.accept;
+
 	$.getJSON(source, function(data) {
 		console.log(data);
 	})
