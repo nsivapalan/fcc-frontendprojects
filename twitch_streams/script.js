7edf6575699da9378f6773f4cb4288c5295e56fe
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2017-01-11 23:53:20 -0800

    update twitch streams with hidden API key

---
+++ twitch_streams/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", 
+								 "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
+
+	var clientID = config.CLIENT_ID;
+	var source;
+	$.getJSON(source, function(data) {
+		console.log(data);
+	})
+
+});
+
+
