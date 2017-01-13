37cdfae0715762af933040edfae3b83f82beca7a
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2017-01-13 15:40:43 -0800

    update all files to get list of live twitch streams

--- twitch_streams/script.js
+++ twitch_streams/script.js
@@ -9,11 +9,36 @@ $(document).ready(function() {
 		"client_id": clientID
 	}
 
-	var source = "https://api.twitch.tv/kraken/channels/freecodecamp?client_id=" + clientID + "&accept" + headers.accept;
+	var original = "https://api.twitch.tv/kraken/streams/"
+	var source = "https://api.twitch.tv/kraken/streams/";
+	var change = "?client_id=" + headers.client_id  + "&accept" + headers.accept;
 
-	$.getJSON(source, function(data) {
-		console.log(data);
-	})
+	// main
+	for (var i in usernames) {
+		source = original;
+		source += usernames[i];
+		source += change;
+		// source = changeSource(i);
+		getInfo();
+	}
+
+	// function changeSource(name) {
+	// 	source = original;
+	// 	source += usernames[name];
+	// 	source += change;
+	// }
+
+	function getInfo() {
+		$.getJSON(source, function(data) {
+			console.log(data);
+			if (data["stream"] !== null) {
+				$("#results").append("<a target='_blank' align='left' href='" + data["stream"]["channel"]["url"] + "''>" 
+					+ "<li><img class='pic' src='" + data["stream"]["channel"]["logo"] + "' />" 
+					+ data["stream"]["channel"]['display_name'] + "<br><small>" + data["stream"]["channel"]['status'] + "</small></li></a><br>");
+			}
+		});
+	}
+
 
 });
 
