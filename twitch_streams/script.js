6d83f654b0c8632a52eae1973c0c9329e7e57654
Nimalen Sivapalan <nsivapalan@berkeley.edu>
2017-01-16 13:30:12 -0800

    change getJSON to ajax requests and remove duplicates

--- twitch_streams/script.js
+++ twitch_streams/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var usernames = ["esl_sc2", "ogamingsc2", "cretetion", "freecodecamp", 
-								 "storbeck", "habathcx", "robotcaleb", "noobs2ninjas", "brunofin", "comster404", "lvagewgweagwg32523"];
+								 "storbeck", "habathcx", "robotcaleb", "noobs2ninjas", "brunofin", "comster404"];
 
  	var isLive;	
 	var name;
@@ -12,18 +12,11 @@ $(document).ready(function() {
 		"client_id": clientID
 	}
 
-	/*
-	Channel info
-	Get the information of a channel like the name, logo, URL, etc. 
-	*/
 	var channelOriginal = "https://api.twitch.tv/kraken/channels/"
 	var channelSource = "https://api.twitch.tv/kraken/channels/";
 	var channelChange = "?client_id=" + headers.client_id  + "&accept" + headers.accept;
 
-	/*
-	Live status info
-	Get the live status of a channel.
-	*/
+
 	var original = "https://api.twitch.tv/kraken/streams/"
 	var source = "https://api.twitch.tv/kraken/streams/";
 	var change = "?client_id=" + headers.client_id  + "&accept" + headers.accept;
@@ -33,7 +26,6 @@ $(document).ready(function() {
 
 		for (var i = 0; i < usernames.length; i++) {
 			name = usernames[i];
-			console.log(name);
 			changeLiveSource(name);
 			getLiveInfo();
 			if (!isLive) {
@@ -83,36 +75,51 @@ $(document).ready(function() {
 		source += name + change;
 	}
 
-	function getChannelInfo() {
-		$.getJSON(channelSource, function(data) {
-			console.log(data);
-			$("#results").append("<a target='_blank' href='" + data['url'] + "''>" 
-				+ "<li class='offline'><img class='pic' src='" + data['logo'] + "' />" 
-				+ "<h2>" + data['name'] + "</h2>"
-				+ "<p>Offline</p>" + "</li></a><br>");
+	/*
+	Channel info
+	Get the information of a channel like the name, logo, URL, etc. 
+	*/
+	function getLiveInfo() {
+		$.ajax({
+			type: 'GET',
+			url: source,
+			success: function(data) {
+				if (data["stream"] !== null) {
+					isLive = true;
+					$("#results").append("<a target='_blank' href='" + data["stream"]["channel"]["url"] + "''>" 
+						+ "<li class='online'><img class='pic' src='" + data["stream"]["channel"]["logo"] + "' />" 
+						+ "<h2>" + data["stream"]["channel"]['display_name'] + "</h2>" 
+						+ "<p><strong>Currently streaming</strong>: " + data["stream"]["channel"]['status'] 
+						+ "</p></li></a><br>");
+				} else {
+					isLive = false;
+				}
+			},
+			async: false // to go in order
 		})
-		.fail(function() {
-			$("#results").append("<li class='inactive'><img class='pic' src='https://s9.postimg.org/w8q60kdr3/inactive.gif' />" 
-				+ "<h2>" + name + "</h2>"
-				+ "<p>This account does not exist.</p>" + "</li></a><br>");
-			console.log("name is " + name);
-		});
 	}
 
-	function getLiveInfo() {
-		$.getJSON(source, function(data) {
-			console.log(data);
-			if (data["stream"] !== null) {
-				isLive = true;
-				$("#results").append("<a target='_blank' href='" + data["stream"]["channel"]["url"] + "''>" 
-					+ "<li class='online'><img class='pic' src='" + data["stream"]["channel"]["logo"] + "' />" 
-					+ "<h2>" + data["stream"]["channel"]['display_name'] + "</h2>" 
-					+ "<p><strong>Currently streaming</strong>: " + data["stream"]["channel"]['status'] 
-					+ "</p></li></a><br>");
-			} else {
-				isLive = false;
+	/*
+	Live status info
+	Get the live status of a channel.
+	*/
+	function getChannelInfo() {
+		$.ajax({
+			type: 'GET',
+			url: channelSource,
+			success: function(data) {
+				$("#results").append("<a target='_blank' href='" + data['url'] + "''>" 
+					+ "<li class='offline'><img class='pic' src='" + data['logo'] + "' />" 
+					+ "<h2>" + data['name'] + "</h2>"
+					+ "<p>Offline</p>" + "</li></a><br>");				
+			},
+			error: function() {
+				$("#results").append("<li class='inactive'><img class='pic' src='https://s9.postimg.org/w8q60kdr3/inactive.gif' />" 
+					+ "<h2>" + name + "</h2>"
+					+ "<p>This account does not exist.</p>" + "</li></a><br>");
+				console.log("name is " + name);
 			}
-		});
+		})
 	}
 
 	main();
